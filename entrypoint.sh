fb077cda56b91a3afb0b498f4bf96f92ad8a3add
kieronellis <69465049+kieronellis@users.noreply.github.com>
2022-07-08 17:05:01 +0200

    logging and echo myvar

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,11 @@ airflow variables import $3
 
 cp -r /action/* /github/workspace/
 
+ls
+
 python -m pytest dag_validation.py -s -q >> result.log
 my_var=`echo Pytest exited $?`
+echo $my_var
 python alert.py --log_filename=result.log --repo_token=$4
 if [ "$my_var" != "Pytest exited 0" ]; then echo "Pytest did not exit 0" ;fi
 if [ "$my_var" != "Pytest exited 0" ]; then exit 1 ;fi
