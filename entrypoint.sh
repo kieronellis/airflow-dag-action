4b5f005c61d2499418a0eb58acd9126bf1a030f1
kieronellis <69465049+kieronellis@users.noreply.github.com>
2022-07-08 16:24:49 +0200

    logging and error on pytest failure

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,8 @@ airflow variables import $3
 
 cp -r /action/* /github/workspace/
 
-pytest dag_validation.py -s -q >> result.log
+python -m pytest dag_validation.py -s -q >> result.log
+my_var=`echo Pytest exited $?`
 python alert.py --log_filename=result.log --repo_token=$4
+if [ "$my_var" != "Pytest exited 0" ]; then echo "Pytest did not exit 0" ;fi
+if [ "$my_var" != "Pytest exited 0" ]; then exit 1 ;fi
